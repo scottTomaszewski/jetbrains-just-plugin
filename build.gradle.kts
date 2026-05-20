38421acbaddf3b317cb7a411e1c538f521b8169b
Scott Tomaszewski <scott.tomaszewski@finxact.com>
2026-05-20 09:06:45 -0400

    Adds build config changes for local install

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,46 +5,41 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    // Java support
     id("java")
-    // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.9.20"
-    // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.17.4"
-    // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "2.2.0"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "2023.2.1"
-
-    id("org.jetbrains.grammarkit") version "2022.3.2.2"
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.intellij.platform")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.qodana")
+    id("org.jetbrains.grammarkit")
 }
 
 group = properties("pluginGroup")
 version = properties("pluginVersion")
 
-// Configure project's dependencies
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    //localPath.set("/Users/linux_china/tools/ideaIU-2024.2")
-    type.set(properties("platformType"))
+dependencies {
+    intellijPlatform {
+        intellijIdeaUltimate(properties("platformVersion"))
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+        bundledPlugins(listOf("com.jetbrains.sh", "JavaScript"))
+        plugins(listOf("PsiViewer:242.4697"))
+
+        instrumentationTools()
+    }
 }
 
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
     version.set(properties("pluginVersion"))
     groups.set(emptyList())
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
 qodana {
     cachePath.set(projectDir.resolve(".qodana").canonicalPath)
     resultsPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
@@ -52,37 +47,65 @@ qodana {
 
 sourceSets["main"].java.srcDirs("src/main/gen")
 
+intellijPlatform {
+    pluginConfiguration {
+        name = properties("pluginName")
+        version = properties("pluginVersion")
+
+        description = projectDir.resolve("README.md").readText().lines().run {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            if (!containsAll(listOf(start, end))) {
+                throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+            }
+            subList(indexOf(start) + 1, indexOf(end))
+        }.joinToString("\n").run { markdownToHTML(this) }
+
+        changeNotes = provider {
+            with(changelog) {
+                renderItem(
+                    getOrNull(properties("pluginVersion")) ?: getLatest(),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+
+        ideaVersion {
+            sinceBuild = properties("pluginSinceBuild")
+            untilBuild = properties("pluginUntilBuild")
+        }
+    }
+
+    signing {
+        certificateChain = System.getenv("CERTIFICATE_CHAIN")
+        privateKey = System.getenv("PRIVATE_KEY")
+        password = System.getenv("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = System.getenv("PUBLISH_TOKEN")
+        channels = providers.gradleProperty("pluginVersion").map {
+            listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" })
+        }
+    }
+}
 
 tasks {
     generateLexer {
-        // source flex file
         sourceFile.set(project.layout.projectDirectory.file("src/main/grammars/Just.flex"))
-
-        // target directory for lexer
         targetOutputDir.set(project.layout.projectDirectory.dir("src/main/gen/org/mvnsearch/plugins/just/lang/lexer/"))
-
-
-        // if set, plugin will remove a lexer output file before generating new one. Default: false
         purgeOldFiles.set(true)
     }
 
     generateParser {
-        // source bnf file
         sourceFile.set(project.layout.projectDirectory.file("src/main/grammars/Just.bnf"))
-
-        // optional, task-specific root for the generated files. Default: none
         targetRootOutputDir.set(project.layout.projectDirectory.dir("src/main/gen"))
-
-        // path to a parser file, relative to the targetRoot
         pathToParser.set("/org/mvnsearch/plugins/just/parser/JustParserGenerated.java")
-
-        // path to a directory with generated psi files, relative to the targetRoot
         pathToPsiRoot.set("/org/mvnsearch/plugins/just/lang/psi")
-
-        // if set, the plugin will remove a parser output file and psi output directory before generating new ones. Default: false
         purgeOldFiles.set(true)
     }
-    // Set the JVM compatibility versions
+
     properties("javaVersion").let {
         withType<JavaCompile> {
             sourceCompatibility = it
@@ -95,58 +118,5 @@ tasks {
 
     wrapper {
         gradleVersion = properties("gradleVersion")
-    }
-
-    patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
-
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(
-            projectDir.resolve("README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
-
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
-
-        // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            with(changelog) {
-                renderItem(
-                    getOrNull(properties("pluginVersion")) ?: getLatest(),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        })
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token.set(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "jetbrains-just-plugin"
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.9.20"
+        id("org.jetbrains.changelog") version "2.2.0"
+        id("org.jetbrains.grammarkit") version "2022.3.2.2"
+        id("org.jetbrains.qodana") version "2023.2.1"
+    }
+}
+
+plugins {
+    id("org.jetbrains.intellij.platform.settings") version "2.11.0"
+}
